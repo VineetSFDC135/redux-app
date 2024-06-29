026a386d5518f4badb3d41583c847b02996a8924
VineetSFDC135 <vineet.salesforce@gmail.com>
2024-06-29 13:11:54 +0530

    adding modified code to GIT

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. This is the new code added.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>This is the modified code which need to be pushed into GIT.</h1>
     </div>
   );
 }
