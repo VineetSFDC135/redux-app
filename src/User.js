b5cbd54a5b404031f43604c56ad8a7f03a74d89b
VineetSFDC135 <vineet.salesforce@gmail.com>
2024-06-29 13:33:25 +0530

    added new changes in the 4th branch

---
+++ src/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ const user= (e) => {
+
+
+    return(
+        <div>
+            <h1>This is Parent User  component for the App.js </h1>
+        </div>
+    );
+     
+};
+
+export default user;
