ef2970b1791349d8d129989219e2a73936178614
VineetSFDC135 <vineet.salesforce@gmail.com>
2024-06-29 19:07:41 +0530

    modified changes for App.js and User.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import User from './User';
 function App() {
+  const usrLst=[{id:'111',name:'aaa',age:10},
+    {id:'222',name:'bbb',age:20},
+    {id:'333',name:'ccc',age:30}];
   return (
     <div className="App">
      <h1>This is the header App component for User</h1>
      
-     <User></User>
+     <User data={usrLst}></User>
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,10 +1,14 @@
 import React from 'react';
- const user= (e) => {
-
+ const user= (props) => {
+    console.log('Props Data '+props.data);
 
     return(
         <div>
             <h1>This is Parent User  component for the App.js </h1>
+            {props.data.map((user)=>{
+
+                return user.name+' ';
+            })}
         </div>
     );
      
